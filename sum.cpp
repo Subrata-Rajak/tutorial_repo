a566530c15629e747c85516fc2c7d2fa935fbdfc
Subrata Rajak <subrata324cse@gmail.com>
2023-02-14 13:44:09 +0530

    initial commit of git tutorial

---
+++ sum.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class solution
+{
+public:
+};
+
+int main()
+{
+    int testcases;
+    cin >> testcases;
+
+    while (testcases--)
+    {
+    }
+    return 0;
+}
